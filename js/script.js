187f61ad417f5128223e1a6968a9d0de6c3bf365
mahmud <mahmudhasan019323@gmail.com>
2018-05-31 16:57:16 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// var openBtns = document.querySelectorAll('.popup__open');
+// var popUp = document.querySelector('.popup');
+// var closeBtn = document.querySelector('.popup__close');
+
+// openBtns.forEach(function(btn){
+
+//     btn.addEventListener('click', function(e){
+//         // e.preventDefault();
+//         popUp.classList.add('popup--active')
+//     })
+
+// })
+
+// closeBtn.addEventListener('click', function(e){
+//     // e.preventDefault();
+//     popUp.classList.remove('popup--active');
+// })
